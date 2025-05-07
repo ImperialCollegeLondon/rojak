82180b3d62ccac407b9dbc0ee4700635db62424f
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2025-05-07 18:29:20 +0100

    Implement cli function to preprocess madis amdar data and output it as parquet

--- src/rojak/cli/data_interface.py
+++ src/rojak/cli/data_interface.py
@@ -1,7 +1,10 @@
 from enum import StrEnum
-from typing import Annotated
+from pathlib import Path
+from typing import Annotated, Optional
 
 import typer
+
+from rojak.datalib.madis.amdar import MadisAmdarPreprocessor
 
 data_app = typer.Typer()
 
@@ -19,23 +22,61 @@ def retrieve(
             "-s",
             "--source",
             case_sensitive=False,
-            help="Select where data should be retreived from",
+            help="Select where data should be retrieved from",
         ),
     ],
 ): ...
 
+
 @data_app.command()
 def preprocess(
-        source: Annotated[
-            DataSource,
-            typer.Option(
-                "-s",
-                "--source",
-                case_sensitive=False,
-                help="Select where data should be retreived from",
-            ),
-        ],
-): ...
+    source: Annotated[
+        DataSource,
+        typer.Option(
+            "-s",
+            "--source",
+            case_sensitive=False,
+            help="Select where data should be retrieved from",
+        ),
+    ],
+    input_dir: Annotated[
+        Path,
+        typer.Option(
+            "-i",
+            "--input_dir",
+            help="Directory containing files to preprocess",
+            exists=True,
+            dir_okay=True,
+            file_okay=True,
+            readable=True,
+        ),
+    ],
+    output_dir: Annotated[
+        Optional[Path],
+        typer.Option(
+            "-o",
+            "--output_dir",
+            help="Directory to save preprocessed files. If unspecified, it will be the input directory",
+        ),
+    ] = None,
+    glob_pattern: Annotated[
+        Optional[str], typer.Option(help="Glob pattern to select files")
+    ] = None,
+):
+    match source:
+        case "madis-amdar":
+            preprocess_madis_amdar_data(input_dir, output_dir, glob_pattern)
+        case "ukmo-amdar":
+            raise NotImplementedError("Not implemented UKMO AMDAR data preprocessing")
+
+
+def preprocess_madis_amdar_data(
+    input_dir: Path, output_dir: Path | None, glob_pattern: str | None
+):
+    preprocessor: MadisAmdarPreprocessor = MadisAmdarPreprocessor(input_dir, glob_pattern=glob_pattern)
+    if output_dir is None:
+        output_dir = input_dir
+    preprocessor.filter_and_export_as_parquet(output_dir)
 
 
 if __name__ == "__main__":
