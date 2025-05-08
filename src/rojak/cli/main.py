26032e595fc2521e737b5f37eeeac579df1c53f3
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2025-05-08 18:04:59 +0100

    Formatting from running pre-commit

--- src/rojak/cli/main.py
+++ src/rojak/cli/main.py
@@ -2,13 +2,12 @@ import typer
 
 app = typer.Typer()
 
+
 @app.command()
 def turbulence():
     print("HELLO from the other side")
 
+
 @app.command()
 def get_data():
     print("potatoes")
-    
-
-
