2b430db03fadf7b12595624f0eef4f90c7bf7072
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2025-05-09 12:47:41 +0100

    Ported over interface to get ERA5 data (not tested)

--- src/rojak/datalib/ecmwf/constants.py
+++ src/rojak/datalib/ecmwf/constants.py
@@ -1,10 +1,18 @@
 from typing import Dict
 
-DATA_SET_NAME: str = "reanalysis-era5-pressure-levels"
-CAT_DATA_DEFAULT: Dict = {
-    "product_type": [
-        "reanalysis"
-    ],
+reanalysis_dataset_names: dict[str, str] = {
+    "pressure_level": "reanalysis-era5-pressure-levels",
+    "single-level": "reanalysis-era5-single-levels",
+}
+
+blank_default: Dict = {
+    "product_type": ["reanalysis"],
+    "data_format": "netcdf",
+    "download_format": "unarchived",
+}
+
+cat_data_default: Dict = {
+    "product_type": ["reanalysis"],
     "variable": [
         "divergence",
         "geopotential",
@@ -14,7 +22,7 @@ CAT_DATA_DEFAULT: Dict = {
         "v_component_of_wind",
         "vertical_velocity",
         "potential_vorticity",
-        "vorticity"
+        "vorticity",
     ],
     # Needs year, month, day, time to be specified
     "pressure_level": [
@@ -30,24 +38,23 @@ CAT_DATA_DEFAULT: Dict = {
     #    "500"
     # ],
     "data_format": "netcdf",
-    "download_format": "unarchived"
+    "download_format": "unarchived",
 }
 
-SINGLE_LEVEL_DATASET_NAME: str = "reanalysis-era5-single-levels"
-SURFACE_DATA_CONTRAILS_DEFAULT: dict = {
+surface_data_contrails_default: dict = {
     "product_type": ["reanalysis"],
     "variable": [
         "surface_pressure",
         "surface_solar_radiation_downwards",
         "top_net_solar_radiation",
-        "top_net_thermal_radiation"
+        "top_net_thermal_radiation",
     ],
     "data_format": "netcdf",
-    "download_format": "unarchived"
+    "download_format": "unarchived",
 }
 
 # Contains all variables for both CAT and contrails that are on the pressure level
-CONTRAIL_CAT_DATA_DEFAULT: dict = {
+contrail_cat_data_default: dict = {
     "product_type": ["reanalysis"],
     "variable": [
         "divergence",
@@ -60,20 +67,50 @@ CONTRAIL_CAT_DATA_DEFAULT: dict = {
         "temperature",
         "u_component_of_wind",
         "v_component_of_wind",
-        "vertical_velocity"
+        "vertical_velocity",
     ],
     # From ground to upper aircraft limit for flight optimisation
     "pressure_level": [
-        "125", "150", "175",
-        "200", "225", "250",
-        "300", "350", "400",
-        "450", "500", "550",
-        "600", "650", "700",
-        "750", "775", "800",
-        "825", "850", "875",
-        "900", "925", "950",
-        "975", "1000"
+        "125",
+        "150",
+        "175",
+        "200",
+        "225",
+        "250",
+        "300",
+        "350",
+        "400",
+        "450",
+        "500",
+        "550",
+        "600",
+        "650",
+        "700",
+        "750",
+        "775",
+        "800",
+        "825",
+        "850",
+        "875",
+        "900",
+        "925",
+        "950",
+        "975",
+        "1000",
     ],
     "data_format": "netcdf",
-    "download_format": "unarchived"
+    "download_format": "unarchived",
 }
+
+data_defaults: Dict[str, Dict] = {
+    "cat": cat_data_default,
+    "surface": surface_data_contrails_default,
+    "contrail": contrail_cat_data_default,
+    "blank": blank_default,
+}
+
+all_hours: list[str]  = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", 
+                         "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", 
+                         "20:00", "21:00", "22:00", "23:00"]  # fmt: skip
+
+six_hourly: list[str] = ["00:00", "06:00", "12:00", "18:00"]  # fmt: skip
