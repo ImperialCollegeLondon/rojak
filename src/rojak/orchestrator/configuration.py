de6f4666096db54de2001e23670be7a85ea52fb1
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2025-05-09 17:21:23 +0100

    Added the configs to the context class

--- src/rojak/orchestrator/configuration.py
+++ src/rojak/orchestrator/configuration.py
@@ -1,11 +1,6 @@
 from pydantic import BaseModel
 
 
-class Context(BaseModel):
-    # Root configuration
-    ...
-
-
 class TurbulenceConfig(BaseModel):
     # Config for turbulence analysis
     ...
@@ -19,3 +14,10 @@ class ContrailsConfig(BaseModel):
 class DataConfig(BaseModel):
     # Config for data, this would cover both observational data and weather data
     ...
+
+
+class Context(BaseModel):
+    # Root configuration
+    turbulence_config: TurbulenceConfig | None = None
+    contrails_config: ContrailsConfig | None = None
+    data_config: DataConfig
