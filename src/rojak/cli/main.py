bb9700f0db952eb25db4cf427cdd7369b8863726
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2025-05-09 16:58:03 +0100

    Implement plumbing on CLI to take a config file as input

--- src/rojak/cli/main.py
+++ src/rojak/cli/main.py
@@ -1,6 +1,11 @@
+from typing import Annotated, TYPE_CHECKING
+
 import typer
 
 from rojak.cli import data_interface
+
+if TYPE_CHECKING:
+    from pathlib import Path
 
 app = typer.Typer()
 app.add_typer(data_interface.data_app, name="data")
@@ -9,6 +14,22 @@ app.add_typer(data_interface.data_app, name="data")
 @app.command()
 def turbulence():
     print("HELLO from the other side")
+
+
+@app.command()
+def run(
+    config_file: Annotated[
+        "Path",
+        typer.Argument(
+            help="Path to configuration file",
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            readable=True,
+            resolve_path=True,
+        ),
+    ],
+) -> None: ...
 
 
 @app.command()
