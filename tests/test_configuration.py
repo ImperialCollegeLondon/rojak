f64508963db621b262bc8beeef020304c156879f
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2025-05-12 10:41:40 +0100

    Added test for configuration file and use Pydantic to instantiate Context class

---
+++ tests/test_configuration.py
@@ -0,0 +1,29 @@
+import pytest
+import yaml
+from typing import TYPE_CHECKING
+
+from rojak.orchestrator import configuration
+from rojak.orchestrator.configuration import DataConfig
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+@pytest.fixture
+def basic_context_yaml_file(tmp_path_factory) -> "Path":
+    content: dict = {"data_config": {"name": "test"}, "name": "test"}
+    output_file: "Path" = tmp_path_factory.mktemp("config") / "simplest_config.yml"
+    with open(output_file, "w") as file:
+        yaml.safe_dump(content, file, encoding="utf-8")
+
+    return output_file
+
+
+def test_context_from_yaml_basic(basic_context_yaml_file) -> None:
+    context = configuration.Context.from_yaml(basic_context_yaml_file)
+
+    assert isinstance(context, configuration.Context)
+    assert context.name == "test"
+    assert isinstance(context.data_config, DataConfig)
+    assert context.turbulence_config is None
+    assert context.contrails_config is None
