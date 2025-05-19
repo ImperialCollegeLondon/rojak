7fc5ac0b9b4d97cc97b593e8d9c889d7a929ce8a
Hui Ling Wong <huilingw312@gmail.com>
2025-05-19 16:39:08 +0100

    Added test for cli data command that throws NotImplementedError

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+import pytest
 from typer.testing import CliRunner
 
 from rojak.cli.main import app
@@ -15,3 +16,43 @@ def test_get_data_command() -> None:
     result = runner.invoke(app, ["get-data"])
     assert result.exit_code == 0
     assert "potatoes" in result.stdout
+
+
+def test_retrieve_data_ukmo_amdar(tmp_path) -> None:
+    with pytest.raises(NotImplementedError) as excinfo:
+        runner.invoke(
+            app,
+            [
+                "data",
+                "retrieve",
+                "-s",
+                "ukmo-amdar",
+                "-y",
+                "2024",
+                "-m",
+                "-1",
+                "-d",
+                "-1",
+                "-o",
+                str(tmp_path),
+            ],
+            catch_exceptions=False,
+        )
+    assert excinfo.type is NotImplementedError
+
+
+def test_preprocess_data_ukmo_amdar(tmp_path) -> None:
+    with pytest.raises(NotImplementedError) as excinfo:
+        runner.invoke(
+            app,
+            [
+                "data",
+                "preprocess",
+                "-s",
+                "ukmo-amdar",
+                "-i",
+                str(tmp_path),
+            ],
+            catch_exceptions=False,
+        )
+    assert excinfo.type is NotImplementedError
