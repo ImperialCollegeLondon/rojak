202c3f726b3c6c46b42ee60b1816a6964cbefab9
Hui Ling Wong <huilingw312@gmail.com>
2025-05-19 14:39:08 +0100

    Add test case where validator needs to create the directories

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -27,11 +27,50 @@ def basic_context_yaml_file(tmp_path_factory) -> "Path":
     return output_file
 
 
-def test_context_from_yaml_basic(basic_context_yaml_file) -> None:
-    context = configuration.Context.from_yaml(basic_context_yaml_file)
+@pytest.fixture
+def basic_context_yaml_file_create_output_plots_on_validation(
+    tmp_path_factory,
+) -> "Path":
+    # Prevent creating the folders in the folder where tests are run
+    tmp_folder: Path = tmp_path_factory.mktemp("empty_folder")
+    content: dict = {
+        "data_config": {"name": "test"},
+        "name": "test",
+        "image_format": "png",
+        "output_dir": str(tmp_folder / "output"),
+        "plots_dir": str(tmp_folder / "plots"),
+    }
+    output_file: "Path" = tmp_path_factory.mktemp("config") / "simplest_config.yml"
+    with open(output_file, "w") as file:
+        yaml.safe_dump(content, file, encoding="utf-8")
+
+    return output_file
+
+
+@pytest.mark.parametrize("is_created", [True, False])
+def test_context_from_yaml_basic(
+    basic_context_yaml_file,
+    basic_context_yaml_file_create_output_plots_on_validation,
+    is_created,
+):
+    if is_created:
+        context = configuration.Context.from_yaml(basic_context_yaml_file)
+    else:
+        context = configuration.Context.from_yaml(
+            basic_context_yaml_file_create_output_plots_on_validation
+        )
 
     assert isinstance(context, configuration.Context)
     assert context.name == "test"
     assert isinstance(context.data_config, DataConfig)
     assert context.turbulence_config is None
     assert context.contrails_config is None
+    assert context.plots_dir.exists() and context.plots_dir.is_dir()
+    assert context.output_dir.exists() and context.output_dir.is_dir()
+
+    if is_created:
+        assert context.plots_dir.name.startswith("plots")
+        assert context.output_dir.name.startswith("output")
+    else:
+        assert context.plots_dir.name == "plots"
+        assert context.output_dir.name == "output"
