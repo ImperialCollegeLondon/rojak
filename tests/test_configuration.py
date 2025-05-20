cad8b89e391111a5ae05a0c0b8ea1334ae27cb0d
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2025-05-20 09:04:38 +0100

    Added tests for the spatial domain model which would define what region the analysis should be preformed on

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -3,7 +3,11 @@ import yaml
 from typing import TYPE_CHECKING
 
 from rojak.orchestrator import configuration
-from rojak.orchestrator.configuration import DataConfig
+from rojak.orchestrator.configuration import (
+    DataConfig,
+    InvalidConfiguration,
+    SpatialDomain,
+)
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -14,7 +18,7 @@ def basic_context_yaml_file(tmp_path_factory) -> "Path":
     tmp_output: Path = tmp_path_factory.mktemp("output")
     tmp_plots: Path = tmp_path_factory.mktemp("plots")
     content: dict = {
-        "data_config": {"name": "test"},
+        "data_config": {"name": "test", "spatial_domain": {}},
         "name": "test",
         "image_format": "png",
         "output_dir": str(tmp_output),
@@ -34,7 +38,7 @@ def basic_context_yaml_file_create_output_plots_on_validation(
     # Prevent creating the folders in the folder where tests are run
     tmp_folder: Path = tmp_path_factory.mktemp("empty_folder")
     content: dict = {
-        "data_config": {"name": "test"},
+        "data_config": {"name": "test", "spatial_domain": {}},
         "name": "test",
         "image_format": "png",
         "output_dir": str(tmp_folder / "output"),
@@ -75,3 +79,106 @@ def test_context_from_yaml_basic(
     else:
         assert context.plots_dir.name == "plots"
         assert context.output_dir.name == "output"
+
+    assert context.data_config.spatial_domain.minimum_latitude == -90
+    assert context.data_config.spatial_domain.maximum_latitude == 90
+    assert context.data_config.spatial_domain.minimum_longitude == -180
+    assert context.data_config.spatial_domain.maximum_longitude == 180
+
+
+# Make this method more generic if needed (i.e. dumps for any dictionary)
+@pytest.fixture
+def spatial_domain_file(request, tmp_path) -> "Path":
+    output_file: Path = tmp_path / "spatial_domain.yml"
+    with open(output_file, "w") as file:
+        yaml.safe_dump(request.param, file, encoding="utf-8")
+    return output_file
+
+
+@pytest.mark.parametrize(
+    "spatial_domain_file",
+    [
+        {
+            "minimum_latitude": 90,
+            "maximum_latitude": -90,
+            "minimum_longitude": 0,
+            "maximum_longitude": 180,
+        },
+        {
+            "minimum_latitude": 0,
+            "maximum_latitude": 90,
+            "minimum_longitude": 90,
+            "maximum_longitude": 0,
+        },
+        {
+            "minimum_latitude": -91,
+            "maximum_latitude": 90,
+            "minimum_longitude": 0,
+            "maximum_longitude": 90,
+        },
+        {
+            "minimum_latitude": 0,
+            "maximum_latitude": 91,
+            "minimum_longitude": 0,
+            "maximum_longitude": 90,
+        },
+        {
+            "minimum_latitude": 0,
+            "maximum_latitude": 90,
+            "minimum_longitude": -181,
+            "maximum_longitude": 0,
+        },
+        {
+            "minimum_latitude": 0,
+            "maximum_latitude": 90,
+            "minimum_longitude": 0,
+            "maximum_longitude": 181,
+        },
+    ],
+    indirect=True,
+)
+def test_spatial_domain_invalid_config(spatial_domain_file) -> None:
+    with pytest.raises(InvalidConfiguration) as excinfo:
+        configuration.SpatialDomain.from_yaml(spatial_domain_file)
+    assert excinfo.type is InvalidConfiguration
+
+
+@pytest.mark.parametrize(
+    "spatial_domain_file",
+    [
+        {
+            "minimum_latitude": -90,
+            "maximum_latitude": 90,
+            "minimum_longitude": 0,
+            "maximum_longitude": 180,
+        },
+        {
+            "minimum_latitude": 0,
+            "maximum_latitude": 90,
+            "minimum_longitude": 0,
+            "maximum_longitude": 90,
+        },
+        {
+            "minimum_latitude": -90,
+            "maximum_latitude": 90,
+            "minimum_longitude": 0,
+            "maximum_longitude": 90,
+        },
+        {
+            "minimum_latitude": 0,
+            "maximum_latitude": 90,
+            "minimum_longitude": 0,
+            "maximum_longitude": 90,
+        },
+        {
+            "minimum_latitude": 0,
+            "maximum_latitude": 90,
+            "minimum_longitude": 0,
+            "maximum_longitude": 180,
+        },
+    ],
+    indirect=True,
+)
+def test_spatial_domain_valid_config(spatial_domain_file) -> None:
+    spatial_domain = SpatialDomain.from_yaml(spatial_domain_file)
+    assert spatial_domain.minimum_longitude == 0
