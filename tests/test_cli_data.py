352597c47852a99ade3ebe921d913454dabc29c0
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2025-05-21 14:51:33 +0100

    Added tests and command to call Era5 data retriever

--- tests/test_cli_data.py
+++ tests/test_cli_data.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING, Tuple
 import pytest
 
 from rojak.cli.main import app
+from rojak.datalib.ecmwf.era5 import InvalidEra5RequestConfiguration
 from tests.test_cli import runner
 
 if TYPE_CHECKING:
@@ -156,3 +157,56 @@ def test_preprocess_data_madis_single_file(retrieve_madis_data_single_file) -> N
     )
     assert result.exit_code == 0
     assert (input_path / "2024" / "01" / "20240101_0000.parquet").exists()
+
+
+@pytest.mark.parametrize(
+    "data_set_name, default_name, matches",
+    [
+        pytest.param(
+            "nonsense",
+            None,
+            "Invalid dataset name",
+            id="invalid_dataset_name",
+        ),
+        pytest.param(
+            "pressure-level",
+            "nonsense",
+            "Invalid default name",
+            id="invalid_default_name",
+        ),
+        pytest.param(
+            "pressure-level",
+            None,
+            "Default not specified",
+            id="default_not_specified",
+        ),
+    ],
+)
+def test_retrieve_meteorology_era5_invalid_config(
+    data_set_name, default_name, matches, tmp_path
+) -> None:
+    with pytest.raises(InvalidEra5RequestConfiguration, match=matches) as excinfo:
+        runner.invoke(
+            app,
+            [
+                "data",
+                "meteorology",
+                "retrieve",
+                "-s",
+                "era5",
+                "-y",
+                "2024",
+                "-m",
+                "1",
+                "-d",
+                "1",
+                "-n",
+                data_set_name,
+                "-o",
+                str(tmp_path),
+                "--default-name",
+                default_name,
+            ],
+            catch_exceptions=False,
+        )
+    assert excinfo.type is InvalidEra5RequestConfiguration
