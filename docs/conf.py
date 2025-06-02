4ee98a42d8a0321fa2b2d1e422e6d12c07595e90
Hui Ling Wong <huilingw312@gmail.com>
2025-06-02 16:06:30 +0100

    Added some sphinx extension and configuration

--- docs/conf.py
+++ docs/conf.py
@@ -13,11 +13,25 @@ author = "Hui Ling Wong"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",  # Include documentation from docstrings
+    "sphinx.ext.apidoc",  # Generate API documentation from Python packages
+    "sphinx.ext.autosummary",  # Generate autodoc summaries
+    "sphinx.ext.napoleon",  # Support for NumPy and Google style docstrings
+    "myst_parser",  # Markdown support
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# Mapping to other project documentation
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
+
+# Allow markdown files to be recognised
+source_suffix = [".rst", ".md"]
+
+# Related to sphinx.ext.autodoc
+apidoc_modules = [{"path": "../src/rojak", "destination": "api/"}]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
