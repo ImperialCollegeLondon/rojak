2a37ec89a55b15bce083127abbcd1849a1435e64
Hui Ling Wong <huilingw312@gmail.com>
2025-06-13 10:06:23 +0100

    Added convenience method to create the geopandas dataframe for the grid

--- src/rojak/core/geometric.py
+++ src/rojak/core/geometric.py
@@ -1,11 +1,15 @@
 import itertools
 from typing import TYPE_CHECKING
 
+import dask_geopandas as dgpd
+import geopandas as gpd
 import numpy as np
 from shapely import geometry
 from shapely.prepared import prep
 
 if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
     from rojak.orchestrator.configuration import SpatialDomain
 
 
@@ -13,11 +17,11 @@ def _create_grid_boxes(bounding_box: geometry.Polygon, step_size: float) -> list
     # Modified from
     # https://www.matecdev.com/posts/shapely-polygon-gridding.html
     min_x, min_y, max_x, max_y = bounding_box.bounds
-    nx = int(np.ceil((max_x - min_x) / step_size))
-    ny = int(np.ceil((max_y - min_y) / step_size))
+    nx: int = int(np.ceil((max_x - min_x) / step_size))
+    ny: int = int(np.ceil((max_y - min_y) / step_size))
 
-    x_loc = np.linspace(min_x, max_x, nx + 1)
-    y_loc = np.linspace(min_y, max_y, ny + 1)
+    x_loc: "NDArray" = np.linspace(min_x, max_x, nx + 1)
+    y_loc: "NDArray" = np.linspace(min_y, max_y, ny + 1)
     return [
         geometry.box(x_loc[x_index], y_loc[y_index], x_loc[x_index + 1], y_loc[y_index + 1])
         for x_index, y_index in itertools.product(range(nx), range(ny))
@@ -34,3 +38,15 @@ def create_rectangular_spatial_grid_buckets(domain: "SpatialDomain", step_size: 
 def create_polygon_spatial_grid_buckets(domain: geometry.Polygon, step_size: float) -> list[geometry.Polygon]:
     prepared_geometry = prep(domain)
     return list(filter(prepared_geometry.intersects, _create_grid_boxes(domain, step_size)))
+
+
+def create_grid_data_frame(
+    domain: "SpatialDomain | geometry.Polygon", step_size: float, crs: str = "epsg:4326"
+) -> dgpd.GeoDataFrame:
+    df = gpd.GeoDataFrame(
+        geometry=create_polygon_spatial_grid_buckets(domain, step_size)
+        if isinstance(domain, geometry.Polygon)
+        else create_rectangular_spatial_grid_buckets(domain, step_size),
+        crs=crs,
+    )
+    return dgpd.from_geopandas(df)
