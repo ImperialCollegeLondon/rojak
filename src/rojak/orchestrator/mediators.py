62019ede709a427e0c6527848c203c874c1a601c
Hui Ling Wong <huilingw312@gmail.com>
2025-06-16 17:00:38 +0100

    WIP working through how to compare amdar and computed diagnostics - mainly put in the plumbing to clip observational data to the time window in the met data

--- src/rojak/orchestrator/mediators.py
+++ src/rojak/orchestrator/mediators.py
@@ -1,8 +1,13 @@
 from typing import TYPE_CHECKING
 
+import numpy as np
+
 if TYPE_CHECKING:
+    import dask.dataframe as dd
+
     from rojak.core.data import AmdarTurbulenceData
     from rojak.turbulence.diagnostic import DiagnosticSuite
+    from rojak.utilities.types import Limits
 
 
 class DiagnosticsAmdarComparator:
@@ -16,3 +21,25 @@ class DiagnosticsAmdarComparator:
     def __init__(self, amdar_data: "AmdarTurbulenceData", diagnostics_suite: "DiagnosticSuite") -> None:
         self._amdar_data = amdar_data
         self._diagnostics_suite = diagnostics_suite
+
+    def _process_amdar_row(self, row: "dd.Series", action: str) -> "dd.Series":
+        return row
+
+    def compute_action(self, action: str, time_window: "Limits[np.datetime64]") -> "dd.DataFrame":
+        observational_data: "dd.DataFrame" = self._amdar_data.clip_to_time_window(time_window)
+        meta_for_new_dataframe = {
+            "datetime": np.datetime64,
+            "level": float,
+            "geometry": object,
+            "grid_box": object,
+            "index_right": int,
+        }
+        for item in self._diagnostics_suite.diagnostic_names():
+            meta_for_new_dataframe[item] = float
+
+        return observational_data.apply(
+            self._process_amdar_row,
+            axis=1,
+            meta=meta_for_new_dataframe,
+            args=action,
+        )
