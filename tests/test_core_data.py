b70cbaccd1f2719912092c9e26b5f668e701bdfc
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2025-06-19 12:23:30 +0100

    Moved a fixture out into conftest so that it can be used in mediator tests

--- tests/test_core_data.py
+++ tests/test_core_data.py
@@ -1,6 +1,5 @@
-from typing import TYPE_CHECKING, Tuple
+from typing import TYPE_CHECKING
 
-import dask.array as da
 import dask.dataframe as dd
 import dask_geopandas
 import numpy as np
@@ -15,20 +14,12 @@ from rojak.datalib.ecmwf.era5 import Era5Data
 from rojak.datalib.madis.amdar import AcarsAmdarRepository
 from rojak.datalib.ukmo.amdar import UkmoAmdarRepository
 from rojak.orchestrator.configuration import SpatialDomain
+from tests.conftest import generate_array_data, time_coordinate
 
 if TYPE_CHECKING:
     from pytest_mock import MockerFixture
 
     from rojak.utilities.types import Limits
-
-
-def time_coordinate():
-    return np.arange("2005-02-01T00", "2005-02-02T00", dtype="datetime64[h]")
-
-
-def generate_array_data(shape: Tuple, use_numpy: bool, rng_seed=None):
-    data = np.random.default_rng(rng_seed).random(shape)
-    return data if use_numpy else da.from_array(data)
 
 
 @pytest.fixture
@@ -55,30 +46,6 @@ def make_select_domain_dummy_data():
         )
 
     return _make_select_domain_dummy_data
-
-
-@pytest.fixture
-def make_dummy_cat_data():
-    def _make_dummy_cat_data(to_replace: dict, use_numpy: bool = True, rng_seed=None) -> xr.Dataset:
-        default_coords = {
-            "longitude": np.arange(10),
-            "latitude": np.arange(10),
-            "time": time_coordinate(),
-            "pressure_level": np.arange(4),
-        }
-        if to_replace:
-            default_coords.update(to_replace)
-        data_vars = {
-            var: xr.DataArray(
-                data=generate_array_data((10, 10, 24, 4), use_numpy, rng_seed),
-                dims=["longitude", "latitude", "time", "pressure_level"],
-            )
-            for var in CATPrognosticData.required_variables
-        }
-        ds = xr.Dataset(data_vars=data_vars, coords=default_coords)
-        return ds.assign_coords(altitude=("pressure_level", np.arange(4)))
-
-    return _make_dummy_cat_data
 
 
 def test_select_spatial_domain_no_slicing(make_select_domain_dummy_data):
