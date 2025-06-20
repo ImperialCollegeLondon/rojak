706dd611c719d86716184abfeca4436f2ab95f17
Hui Ling Wong <huilingw312@gmail.com>
2025-06-20 16:42:57 +0100

    Implemented and added test for a function which determines the spacing on a regular grid. If it is not a regular grid, None is returned

--- tests/test_core_indexing.py
+++ tests/test_core_indexing.py
@@ -1,9 +1,12 @@
-from typing import Sequence
+from typing import TYPE_CHECKING, Sequence
 
 import numpy as np
 import pytest
 
-from rojak.core.indexing import make_value_based_slice
+from rojak.core.indexing import get_regular_grid_spacing, make_value_based_slice
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
 
 
 @pytest.mark.parametrize(
@@ -24,3 +27,31 @@ def test_make_value_based_slice(
 ) -> None:
     computed_slice = make_value_based_slice(coordinate, min_value, max_value)
     assert computed_slice == expected_slice
+
+
+@pytest.mark.parametrize(
+    ("array", "expected"),
+    [
+        (np.arange(10), 1),
+        (np.arange(10, 0, -1), -1),
+        (np.arange(0, -10, -1), -1),
+        (np.arange(-10, 1, 1), 1),
+        (np.arange(0, 10, 1.0 / 3), 1 / 3),
+        (np.linspace(5, 10, 11), 0.5),
+        (np.linspace(10, 5, 11), -0.5),
+        (
+            np.arange(np.datetime64("1970-01-01"), np.datetime64("1970-01-02"), dtype="datetime64[h]"),
+            np.timedelta64(1, "h"),
+        ),
+        (
+            np.arange(
+                np.datetime64("1970-01-01"), np.datetime64("1970-01-02"), np.timedelta64(6, "h"), dtype="datetime64[h]"
+            ),
+            np.timedelta64(6, "h"),
+        ),
+        (np.asarray([3, 5.6, 6]), None),
+    ],
+)
+def test_get_regular_grid_spacing(array: "NDArray", expected: float | None) -> None:
+    spacing = get_regular_grid_spacing(array)
+    assert spacing == expected
