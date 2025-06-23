5b17011ca9dc52a50c1192006d660517535f7673
Hui Ling Wong <huilingw312@gmail.com>
2025-06-23 12:58:56 +0100

    Added method which works out the index into an array based on its value

--- tests/test_core_indexing.py
+++ tests/test_core_indexing.py
@@ -1,11 +1,13 @@
 from typing import TYPE_CHECKING, Sequence
 
 import numpy as np
+import pandas as pd
 import pytest
 
-from rojak.core.indexing import get_regular_grid_spacing, make_value_based_slice
+from rojak.core.indexing import get_regular_grid_spacing, make_value_based_slice, map_values_to_coordinate_index
 
 if TYPE_CHECKING:
+    import dask.dataframe as dd
     from numpy.typing import NDArray
 
 
@@ -66,3 +68,84 @@ def test_get_regular_grid_spacing(array: "NDArray", expected: float | None) -> N
 def test_get_regular_grid_spacing_not_implemented(array: "NDArray") -> None:
     with pytest.raises(NotImplementedError):
         get_regular_grid_spacing(array)
+
+
+@pytest.mark.parametrize(
+    ("series", "array", "window", "expected"),
+    [
+        pytest.param(
+            None, np.eye(3, 3), None, pytest.raises(ValueError, match="Coordinate must be 1D not 2D"), id="not_1d"
+        ),
+        pytest.param(
+            pd.Series(np.arange(10)),
+            np.arange(8),
+            1,
+            pytest.raises(ValueError, match="Values in series must be within the window of the coordinate"),
+            id="outside_window_upper",
+        ),
+        pytest.param(
+            pd.Series(np.arange(11)),
+            np.arange(10),
+            None,
+            pytest.raises(ValueError, match="Values in series must be within the range of the coordinate"),
+            id="outside_strict_window",
+        ),
+        pytest.param(
+            pd.Series(np.arange(10)),
+            np.arange(8) + 2,
+            1,
+            pytest.raises(ValueError, match="Values in series must be within the window of the coordinate"),
+            id="outside_window_lower",
+        ),
+        pytest.param(
+            pd.Series(np.arange(10)),
+            np.arange(5) + 3,
+            1,
+            pytest.raises(ValueError, match="Values in series must be within the window of the coordinate"),
+            id="outside_window_lower_and_upper",
+        ),
+        pytest.param(
+            pd.Series(np.arange(5)),
+            np.asarray([0, 4, 4.5, 4.75]),
+            None,
+            pytest.raises(
+                NotImplementedError,
+                match="Optimisation to map values to index into coordinate is only supported for regular grids",
+            ),
+            id="irregular_grid",
+        ),
+        pytest.param(
+            pd.Series(np.arange(5)),
+            np.asarray([5, 8, 4, -1]),
+            None,
+            pytest.raises(
+                NotImplementedError,
+                match="Optimisation to map values to index into coordinate is only supported for regular grids",
+            ),
+            id="irregular_and_not_monotonic",
+        ),
+        pytest.param(
+            pd.Series(np.arange(10)),
+            np.arange(10),
+            1,
+            pytest.raises(
+                NotImplementedError,
+                match="Function currently only supports regular grids with a symmetric window specified",
+            ),
+            id="window_not_half",
+        ),
+        pytest.param(
+            pd.Series(np.arange(np.datetime64("1970-01-01"), np.datetime64("1970-01-02"), np.timedelta64(2, "h"))),
+            np.arange(np.datetime64("1970-01-01"), np.datetime64("1970-01-02"), np.timedelta64(2, "h")),
+            np.timedelta64(2, "h"),
+            pytest.raises(
+                NotImplementedError,
+                match="Function currently only supports regular grids with a symmetric window specified",
+            ),
+            id="window_not_half_time",
+        ),
+    ],
+)
+def test_map_value_to_coordinate_index_fails(series: "dd.Series", array: "NDArray", window, expected) -> None:
+    with expected:
+        map_values_to_coordinate_index(series, array, valid_window=window)
