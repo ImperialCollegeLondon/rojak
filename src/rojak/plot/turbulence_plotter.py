614620750b2b11ef9ba55cb8e36569966420cbaa
Hui Ling Wong <huilingw312@gmail.com>
2025-06-27 12:41:21 +0100

    Copied over method to create a single turbulence plot

--- src/rojak/plot/turbulence_plotter.py
+++ src/rojak/plot/turbulence_plotter.py
@@ -12,12 +12,15 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Tuple
 
 import cartopy.crs as ccrs
+import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
+import numpy as np
+from shapely import Polygon
 
-from rojak.orchestrator.configuration import TurbulenceDiagnostics
+from rojak.orchestrator.configuration import SpatialDomain, TurbulenceDiagnostics
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -25,6 +28,8 @@ if TYPE_CHECKING:
     import xarray as xr
     from cartopy.mpl.geoaxes import GeoAxes
     from matplotlib.figure import Figure
+    from matplotlib.image import AxesImage
+    from numpy.typing import NDArray
 
 
 _PLATE_CARREE: "ccrs.Projection" = ccrs.PlateCarree()
@@ -50,7 +55,7 @@ def xarray_plot_wrapper(
     plt.tight_layout()
     ax.coastlines()
     plt.savefig(plot_name)
-    plt.close()
+    plt.close(fig)
 
 
 diagnostic_label_mapping: dict[TurbulenceDiagnostics, str] = {
@@ -83,3 +88,50 @@ diagnostic_label_mapping: dict[TurbulenceDiagnostics, str] = {
     TurbulenceDiagnostics.EDR_LUNNON: "EDRLun",
     TurbulenceDiagnostics.VORTICITY_SQUARED: r"$|\zeta^2|$",
 }
+
+
+def calculate_extent(spatial_domain: "SpatialDomain | Polygon") -> Tuple[float, float, float, float]:
+    if isinstance(spatial_domain, SpatialDomain):
+        return (
+            spatial_domain.minimum_longitude,
+            spatial_domain.maximum_longitude,
+            spatial_domain.minimum_latitude,
+            spatial_domain.maximum_latitude,
+        )
+    if isinstance(spatial_domain, Polygon):
+        min_x, min_y, max_x, max_y = spatial_domain.bounds
+        return min_x, max_x, min_y, max_y
+    raise TypeError("SpatialDomain must be of type SpatialDomain or Polygon")
+
+
+def create_turbulence_probability_plot(
+    probabilities: "xr.DataArray",
+    plot_name: str,
+    spatial_domain: "SpatialDomain | Polygon",
+    diagnostic: TurbulenceDiagnostics,
+    projection: "ccrs.Projection" = _PLATE_CARREE,
+    cmap_bounds: "NDArray | None" = None,
+    cmap_name: str | None = None,
+) -> None:
+    fig: "Figure" = plt.figure(figsize=(8, 6))
+    # Passing it the kwarg projection => it is a cartopy GeoAxis
+    ax: "GeoAxes" = fig.add_subplot(1, 1, 1, projection=projection)  # pyright: ignore[reportAssignmentType]
+    extent = calculate_extent(spatial_domain)
+    ax.set_extent(extent)
+    # im: "AxesImage" = ax.imshow(probability, extent=extent, cmap="jet", vmin=0, vmax=1)
+    # 4, 8, 12, 16 20
+
+    if cmap_bounds is None:
+        cmap_bounds = np.arange(21)
+    if cmap_name is None:
+        cmap_name = "WhiteBlueGreenYellowRed"
+    cmap = plt.get_cmap(cmap_name)
+    norm = mcolors.BoundaryNorm(boundaries=cmap_bounds, ncolors=cmap.N, extend="max")
+
+    im: "AxesImage" = ax.imshow(probabilities, extent=extent, cmap=cmap, norm=norm, transform=projection)
+    ax.set_title(diagnostic_label_mapping[diagnostic])
+    fig.colorbar(im, ax=ax, label="Turbulence Percentage", spacing="uniform")
+    ax.coastlines()
+    fig.tight_layout()
+    plt.savefig(plot_name)
+    plt.close()
