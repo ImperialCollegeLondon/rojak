7fcc46d6a85e47f68a8f5dae5ba5553acf46926c
Hui Ling Wong <huilingw312@gmail.com>
2025-06-27 15:37:06 +0100

    Commented out the check to ensure grids match as it is EXPENSIVE

--- tests/test_orchestrator_mediators.py
+++ tests/test_orchestrator_mediators.py
@@ -3,10 +3,7 @@ from typing import TYPE_CHECKING
 import numpy as np
 import pytest
 import xarray as xr
-from shapely import box
 
-from rojak.core.data import expand_grid_bounds
-from rojak.core.geometric import create_grid_data_frame
 from rojak.orchestrator.configuration import TurbulenceDiagnostics, TurbulenceSeverity
 from rojak.orchestrator.mediators import (
     DiagnosticsAmdarDataHarmoniser,
@@ -23,7 +20,6 @@ from tests.conftest import generate_array_data
 
 if TYPE_CHECKING:
     from pytest_mock import MockerFixture
-    from shapely.geometry import Polygon
 
 
 @pytest.mark.parametrize(
@@ -51,48 +47,48 @@ def test_diagnostic_amdar_data_harmoniser_execute_harmonisation_fail_time_window
     computed_vals_mock.assert_called_once()
 
 
-@pytest.mark.parametrize(
-    ("grid_shape", "grid_spacing"),
-    [
-        pytest.param(box(-130, 25, 28, 60), 0.1, id="grid_spacing_too_small"),
-        pytest.param(box(-180, 25, 28, 60), 0.25, id="lower_x_limit_too_low"),
-        pytest.param(box(-130, 0, 28, 60), 0.25, id="lower_y_limit_too_low"),
-        pytest.param(box(-130, 25, 80, 60), 0.25, id="upper_x_limit_too_high"),
-        pytest.param(box(-130, 25, 28, 180), 0.25, id="upper_y_limit_too_high"),
-    ],
-)
-def test_diagnostic_amdar_data_harmoniser_execute_harmonisation_fail_on_grid(
-    grid_shape: "Polygon",
-    grid_spacing: float,
-    mocker: "MockerFixture",
-) -> None:
-    x_axis = np.arange(-130, 28, 0.25)
-    y_axis = np.arange(25, 60, 0.25)
-    representative_array = xr.DataArray(
-        data=np.random.default_rng().random((len(x_axis), len(y_axis))),
-        coords={"longitude": x_axis, "latitude": y_axis},
-    )
-    suite_mock = mocker.Mock()
-    computed_vals_mock = mocker.patch.object(
-        suite_mock, "computed_values_as_dict", return_value={"unused_key": representative_array}
-    )
-    amdar_data_mock = mocker.Mock()
-    mocker.patch.object(
-        amdar_data_mock, "data_frame", new=expand_grid_bounds(create_grid_data_frame(box(-130, 25, 28, 60), 0.25))
-    )
-
-    harmoniser = DiagnosticsAmdarDataHarmoniser(amdar_data_mock, suite_mock)
-    time_window_check_mock = mocker.patch.object(harmoniser, "_check_time_window_within_met_data")
-
-    with pytest.raises(AssertionError) as excinfo:
-        harmoniser.execute_harmonisation(
-            [DiagnosticsAmdarHarmonisationStrategyOptions.EDR],
-            Limits(np.datetime64("1970-01-01"), np.datetime64("1980-01-01")),
-        )
-
-    assert excinfo.type is AssertionError
-    time_window_check_mock.assert_called_once()
-    computed_vals_mock.assert_called_once()
+# @pytest.mark.parametrize(
+#     ("grid_shape", "grid_spacing"),
+#     [
+#         pytest.param(box(-130, 25, 28, 60), 0.1, id="grid_spacing_too_small"),
+#         pytest.param(box(-180, 25, 28, 60), 0.25, id="lower_x_limit_too_low"),
+#         pytest.param(box(-130, 0, 28, 60), 0.25, id="lower_y_limit_too_low"),
+#         pytest.param(box(-130, 25, 80, 60), 0.25, id="upper_x_limit_too_high"),
+#         pytest.param(box(-130, 25, 28, 180), 0.25, id="upper_y_limit_too_high"),
+#     ],
+# )
+# def test_diagnostic_amdar_data_harmoniser_execute_harmonisation_fail_on_grid(
+#     grid_shape: "Polygon",
+#     grid_spacing: float,
+#     mocker: "MockerFixture",
+# ) -> None:
+#     x_axis = np.arange(-130, 28, 0.25)
+#     y_axis = np.arange(25, 60, 0.25)
+#     representative_array = xr.DataArray(
+#         data=np.random.default_rng().random((len(x_axis), len(y_axis))),
+#         coords={"longitude": x_axis, "latitude": y_axis},
+#     )
+#     suite_mock = mocker.Mock()
+#     computed_vals_mock = mocker.patch.object(
+#         suite_mock, "computed_values_as_dict", return_value={"unused_key": representative_array}
+#     )
+#     amdar_data_mock = mocker.Mock()
+#     mocker.patch.object(
+#         amdar_data_mock, "data_frame", new=expand_grid_bounds(create_grid_data_frame(box(-130, 25, 28, 60), 0.25))
+#     )
+#
+#     harmoniser = DiagnosticsAmdarDataHarmoniser(amdar_data_mock, suite_mock)
+#     time_window_check_mock = mocker.patch.object(harmoniser, "_check_time_window_within_met_data")
+#
+#     with pytest.raises(AssertionError) as excinfo:
+#         harmoniser.execute_harmonisation(
+#             [DiagnosticsAmdarHarmonisationStrategyOptions.EDR],
+#             Limits(np.datetime64("1970-01-01"), np.datetime64("1980-01-01")),
+#         )
+#
+#     assert excinfo.type is AssertionError
+#     time_window_check_mock.assert_called_once()
+#     computed_vals_mock.assert_called_once()
 
 
 @pytest.mark.parametrize(
