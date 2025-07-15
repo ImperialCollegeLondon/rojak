a92cc6e0d473def7b3002c09536a2a8fe68872c3
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2025-07-15 11:13:59 +0100

    Implemented method to compute binary classification curve

--- src/rojak/turbulence/verification.py
+++ src/rojak/turbulence/verification.py
@@ -1,5 +1,5 @@
 import functools
-from typing import TYPE_CHECKING, Callable
+from typing import TYPE_CHECKING, Callable, NamedTuple
 
 import dask.array as da
 import dask.dataframe as dd
@@ -21,6 +21,91 @@ if TYPE_CHECKING:
         DiagnosticsAmdarDataHarmoniser,
     )
     from rojak.utilities.types import Limits
+
+
+class BinaryClassificationResult(NamedTuple):
+    false_positives: "da.Array"
+    true_positives: "da.Array"
+    thresholds: "da.Array"
+
+
+# Modified from scikit-learn metric roc's invocation of binary classification method
+# https://github.com/scikit-learn/scikit-learn/blob/da08f3d99194565caaa2b6757a3816eef258cd70/sklearn/metrics/_ranking.py#L826
+def binary_classification_curve(
+    sorted_truth: "da.Array",
+    sorted_values: "da.Array",
+    num_intervals: int = 100,
+    positive_classification_label: int | float | bool | str | None = None,
+) -> BinaryClassificationResult:
+    """
+    Received operating characteristic or ROC curve
+
+    Args:
+        sorted_truth:
+        sorted_values:
+        num_intervals:
+        positive_classification_label:
+
+    Returns:
+        BinaryClassificationResult: A named tuple with the false positive, true positive, and thresholds
+
+    Modifying the example in the scikit-learn documentation:
+    >>> import dask.array as da
+    >>> y = np.asarray([1, 1, 2, 2])
+    >>> scores = np.asarray([0.1, 0.4, 0.35, 0.8])
+    >>> binary_classification_curve(da.asarray(y), da.asarray(scores), positive_classification_label=2)
+    Traceback (most recent call last):
+    ValueError: values must be strictly decreasing
+    >>> decrease_idx = np.argsort(scores)[::-1]
+    >>> scores = da.asarray(scores[decrease_idx])
+    >>> y = da.asarray(y[decrease_idx])
+    >>> classification = binary_classification_curve(y, scores, positive_classification_label=2)
+    >>> classification
+    RocResults(false_positives=dask.array<sub, shape=(nan,), dtype=int64, chunksize=(nan,), chunktype=numpy.ndarray>,
+    true_positives=dask.array<slice_with_int_dask_array_aggregate, shape=(nan,), dtype=int64, chunksize=(nan,),
+    chunktype=numpy.ndarray>, thresholds=dask.array<slice_with_int_dask_array_aggregate, shape=(nan,), dtype=float64,
+    chunksize=(nan,), chunktype=numpy.ndarray>)
+    >>> classification.false_positives.compute()
+    array([0, 1, 1, 2])
+    >>> classification.true_positives.compute()
+    array([1, 1, 2, 2])
+    >>> classification.thresholds.compute()
+    array([0.8 , 0.4 , 0.35, 0.1 ])
+    """
+    if sorted_truth.ndim != 1 or sorted_values.ndim != 1:
+        raise ValueError("sorted_truth and sorted_values must be 1D")
+    if sorted_truth.size != sorted_values.size:
+        raise ValueError("sorted_truth and sorted_values must have same size")
+
+    if sorted_truth.dtype != bool:
+        if positive_classification_label is None:
+            raise ValueError(
+                f"positive_classification_label must be specified if truth array is not bool ({sorted_truth.dtype})"
+            )
+        sorted_truth = sorted_truth == positive_classification_label
+
+    diff_values: da.Array = da.diff(sorted_values)
+    if not da.all(diff_values < 0).compute():
+        raise ValueError("values must be strictly decreasing")
+    diff_values = da.abs(diff_values)
+
+    values_min: float = da.nanmin(sorted_values).compute()
+    values_max: float = da.nanmax(sorted_values).compute()
+    minimum_step_size: float = np.abs(values_max - values_min) / num_intervals
+
+    # As the values would be from the turbulence diagnostics, they are continuous
+    # To reduce the data, use step_size to determine the minimum difference between two data points
+    bucketed_value_indices = da.nonzero(diff_values > minimum_step_size)[0]
+    threshold_indices = da.hstack((bucketed_value_indices, da.asarray([sorted_truth.size - 1])))
+
+    true_positive = da.cumsum(sorted_truth)[threshold_indices]
+    # Magical equation from scikit-learn which means another cumsum is avoided
+    # https://github.com/scikit-learn/scikit-learn/blob/da08f3d99194565caaa2b6757a3816eef258cd70/sklearn/metrics/_ranking.py#L907
+    false_positive = 1 + threshold_indices - true_positive
+
+    return BinaryClassificationResult(
+        false_positives=false_positive, true_positives=true_positive, thresholds=sorted_values[threshold_indices]
+    )
 
 
 def _observed_turbulence_aggregation(condition: "DiagnosticValidationCondition") -> dd.Aggregation:
