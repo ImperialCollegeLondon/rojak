1704b37430327413e71191b7067d22110af054b0
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2026-02-26 13:15:53 +0000

    feat: Plumbing to invoke the thresholds computation from lite interface

--- src/rojak/cli/lite_interface.py
+++ src/rojak/cli/lite_interface.py
@@ -4,8 +4,8 @@ from typing import Annotated
 import typer
 from distributed import Client
 
-from rojak.orchestrator.lite_configuration import TurbulenceContextWithOutput
-from rojak.orchestrator.lite_controller import compute_distribution_parameters
+from rojak.orchestrator.lite_configuration import DiagnosticThresholdsContext, TurbulenceContextWithOutput
+from rojak.orchestrator.lite_controller import compute_distribution_parameters, compute_thresholds
 
 # Root application for this interface
 lite_app = typer.Typer(help="Lite run of rojak for lower memory usage")
@@ -34,4 +34,24 @@ def distribution_parameters(
     client = Client()
     context: TurbulenceContextWithOutput = TurbulenceContextWithOutput.from_yaml(config_file)
     compute_distribution_parameters(context)
+    _ = client.close()
+
+
+@turbulence_app.command()
+def turbulence_thresholds(
+    config_file: Annotated[
+        Path,
+        typer.Argument(
+            help="Path to configuration file",
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            readable=True,
+            resolve_path=True,
+        ),
+    ],
+) -> None:
+    client = Client()
+    context: DiagnosticThresholdsContext = DiagnosticThresholdsContext.from_yaml(config_file)
+    compute_thresholds(context)
     _ = client.close()
