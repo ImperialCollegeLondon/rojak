4d64b741ba25e8ed327f90f3f4952b6a0030b137
Hui Ling Wong <71387295+wong-hl@users.noreply.github.com>
2026-02-26 10:56:21 +0000

    feat: Implemented method to aggregate over entire dataset

--- src/rojak/core/calculations.py
+++ src/rojak/core/calculations.py
@@ -1,6 +1,7 @@
 import copy
 import dataclasses
-from typing import TYPE_CHECKING, Literal
+from enum import StrEnum
+from typing import TYPE_CHECKING, Any, Literal, overload
 
 import numpy as np
 import xarray as xr
@@ -329,3 +330,64 @@ def interpolation_on_lat_lon(
         output_dtypes=[data.dtype],
     )
     return interpolated.assign_coords({points_dim: np.arange(interp_points_shape[0])})
+
+
+class XrAggregationMethod(StrEnum):
+    ALL = "all"
+    ANY = "any"
+    ARGMAX = "argmax"
+    ARGMIN = "argmin"
+    COUNT = "count"
+    IDX_MAX = "idxmax"
+    IDX_MIN = "idxmin"
+    MAX = "max"
+    MIN = "min"
+    MEAN = "mean"
+    MEDIAN = "median"
+    PROD = "prod"
+    SUM = "sum"
+    STDDEV = "std"
+    VARIANCE = "var"
+    CUM_SUM = "cumsum"
+    CUM_PROD = "cumprod"
+
+
+@overload
+def apply_data_var_reduction(
+    data: xr.Dataset,
+    method_name: XrAggregationMethod,
+    *,
+    new_dim: str = "ensemble",
+    var_name: str = "ensemble",
+    append: Literal[False],
+    **kwargs: Any,  # noqa: ANN401
+) -> xr.DataArray: ...
+
+
+@overload
+def apply_data_var_reduction(
+    data: xr.Dataset,
+    method_name: XrAggregationMethod,
+    *,
+    new_dim: str = "ensemble",
+    var_name: str = "ensemble",
+    append: Literal[True] = True,
+    **kwargs: Any,  # noqa: ANN401
+) -> xr.Dataset: ...
+
+
+def apply_data_var_reduction(
+    data: xr.Dataset,
+    method_name: XrAggregationMethod,
+    *,
+    new_dim: str = "ensemble",
+    var_name: str = "ensemble",
+    append: bool = False,
+    **kwargs: Any,
+) -> xr.DataArray | xr.Dataset:
+    as_array: xr.DataArray = data.to_array(dim=new_dim)
+    result = getattr(as_array, method_name)(dim=new_dim, **kwargs)
+
+    if append:
+        return data.assign(**{var_name: result})
+    return result.rename(var_name)
